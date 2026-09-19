c4a7f4a660e2029ec4f99be987c4715b9bfb1034
CjayDoesCode <cjaycodes@protonmail.com>
2026-09-19 20:21:09 +0800

    Add C++ solution for problem 58. Length of Last Word

---
+++ easy/58. Length of Last Word/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0;
+        for (int i = s.size() - 1; i >= 0; --i) {
+            if (len == 0 && s[i] ==  ) {
+                continue;
+            } else if (len > 0 && s[i] ==  ) {
+                return len;
+            } else {
+                ++len;
+            }
+        }
+        return len;
+    }
+};
